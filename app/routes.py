2d61553cf94fafc57aed4a7b91ff2231ebd8bd23
Ronny Mathew <ronnygeomatt@gmail.com>
2016-06-07 12:05:11 -0400

    minor changes.

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# render home page
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
+# rendering the about page
 @app.route('/about')
 def about():
     return render_template('about.html')
